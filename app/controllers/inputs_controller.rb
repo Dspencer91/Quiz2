5d3a926ab8612a2c0306b791aa2611b6c6be4181
dillon spencer <dillonstreetfrontcommercial@gmail.com>
2019-11-21 22:08:41 +0000

    Able to post on form

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -7,4 +7,15 @@ end
   def new
     @input = Input.new
   end
+
+     def create
+    Input.create(input_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def input_params
+    params.require(:input).permit(:name, :description, :address)
+  end
 end
